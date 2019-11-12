de70cd12ffec1c1ffdd0f8bf35b1d7a64ecf1573
rong <rong161514@gmail.com>
2019-11-12 14:12:04 +0800

    新建service用来提示通知，待连服务端

--- app/src/main/java/com/example/tower/RequestForNotification.java
+++ app/src/main/java/com/example/tower/RequestForNotification.java
@@ -2,12 +2,12 @@ package com.example.tower;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
 
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -17,40 +17,39 @@ public class RequestForNotification extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-
-        //TODO fetch some data here
-
-
-        Intent mintent = new Intent(this, MainActivity.class);
+        /*Intent mintent = new Intent(this, MainActivity.class);
         mintent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, mintent, 0);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, mintent, 0);*/
 
         createNotificationChannel();
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.service))
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.channel_id))
                 .setContentTitle("Title")
+                .setSmallIcon(R.drawable.water)
                 .setContentText("This is a very looooooooooooooong notification.")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setAutoCancel(true);  //Required, for support for 7.1 and lower
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
         notificationManagerCompat.notify(80, builder.build());
+
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
         return null;
     }
 
     //Required, for support for 8.0 and higher
-    private void createNotificationChannel() {
+    private void createNotificationChannel() throws NullPointerException {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channel_id = getString(R.string.channel_id);
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_des);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(getString(R.string.service), name, importance);
+            NotificationChannel channel = new NotificationChannel(channel_id, name, importance);
             channel.setDescription(description);
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
