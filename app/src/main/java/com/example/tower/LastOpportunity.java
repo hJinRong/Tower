26a657956af08e96283c2949a1057ee26b7b040c
rong <rong161514@gmail.com>
2019-11-24 15:29:28 +0800

    修改措辞

--- app/src/main/java/com/example/tower/LastOpportunity.java
+++ app/src/main/java/com/example/tower/LastOpportunity.java
@@ -9,8 +9,8 @@ public class LastOpportunity extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent lo = new Intent(context.getApplicationContext(), RequestForNotification.class);
-        lo.putExtra("TITLE", "低电量的最后充值机会");
-        lo.putExtra("CONTENT_TEXT", "余额所剩无多，珍惜充值机会");
+        lo.putExtra("TITLE", "低电量");
+        lo.putExtra("CONTENT_TEXT", "快要没电关机了，这是最后的充值机会");
         context.getApplicationContext().startService(lo);
     }
 }
